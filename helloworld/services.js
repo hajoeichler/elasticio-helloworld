d99b468adc7826c6b12d991c8d7db693b15e9b0c
Hajo Eichler <hajo.eichler@commercetools.de>
2013-07-22 22:44:48 +0200

    Simple elastic example.

---
+++ helloworld/services.js
@@ -0,0 +1,21 @@
+var parseString = require('xml2js').parseString;
+
+exports.init = function(msg, cfg, cb) {
+  console.log('init')
+};
+
+exports.process = function(msg, cfg, next) {
+  console.log('process')
+  var payload = msg.body.payload;
+  if (typeof payload === "string") {
+    console.log(payload);
+    var parseString = require('xml2js').parseString;
+    parseString(payload, function (err, result) {
+      console.dir(result);
+      });
+  }
+};
+
+exports.shutdown = function(msg, cfg, cb, snapshot) {
+  console.log('shutdown')
+};
