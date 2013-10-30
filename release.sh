e3e276028a4eb3710f43e12680ee6956e11abbf5
Hajo Eichler <hajo.eichler@commercetools.de>
2013-10-30 17:31:48 +0100

    Fix version bumping.

--- release.sh
+++ release.sh
@@ -2,18 +2,14 @@
 
 set -e
 
-if [ "$1" = "" ]; then
-    echo "Please provide a commit message."
-    exit 1
-fi
-
 git checkout production
 git merge master
 npm version major
 grunt
 git add -f build/app/services.js
-git commit -m "$1"
+git commit -m "Add generated code for production environment."
 git push origin production
 
 git checkout master
+npm version major
 npm version minor
