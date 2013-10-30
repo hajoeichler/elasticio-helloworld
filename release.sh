96a9a185619be2d148f56cecbbd172931a95618f
Hajo Eichler <hajo.eichler@commercetools.de>
2013-10-30 17:34:13 +0100

    Make versioning even simpler

--- release.sh
+++ release.sh
@@ -2,14 +2,13 @@
 
 set -e
 
+npm version major
 git checkout production
 git merge master
-npm version major
 grunt
 git add -f build/app/services.js
 git commit -m "Add generated code for production environment."
 git push origin production
 
 git checkout master
-npm version major
 npm version minor
